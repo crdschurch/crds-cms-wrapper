f3334ab9f654eb263a7591804d71b25e65397c4c
jjanssen <jjanssen@callibrity.com>
2018-12-04 12:29:26 -0500

    testing auto deploy

--- contentblock/index.js
+++ contentblock/index.js
@@ -17,7 +17,7 @@ module.exports = function(context, req) {
       try {
         context.res = {
           status: response.status,
-          body: cleanBody(response),
+          body: "test",//cleanBody(response),
           headers: {
             "Content-Type": "application/json"
           }
